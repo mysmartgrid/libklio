8aa6795d37cf44c01a0a2611de0bc2e0a7dce11f
Mathias Dalheimer <md@gonium.net>
2011-01-27 11:11:11 +0100

    added first code framework

---
+++ src/klio-add.cpp
@@ -0,0 +1,26 @@
+/**
+ * This file is part of libklio.
+ *
+ * (c) Fraunhofer ITWM - Mathias Dalheimer <dalheimer@itwm.fhg.de>, 2010
+ *
+ * libklio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libklio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libklio. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <libklio/common.hpp>
+
+int main(int argc,char** argv) {
+  klio::VersionInfo::Ptr vi(new klio::VersionInfo());
+  LOG("klio library version " << vi->getVersion());
+}

611d49f2ad6810a481ec8a4441ff5255f755d9b2
Mathias Dalheimer <md@gonium.net>
2012-01-10 10:44:34 +0100

    implemented export routines, need moar work. See #5.

--- libklio/octave_exporter.cpp
+++ libklio/octave_exporter.cpp
@@ -1,15 +1,24 @@
 #include "octave_exporter.hpp"
 #include <boost/algorithm/string.hpp>
+#include <vector>
+
 using namespace boost;
 using namespace klio;
 
-static const uint16_t SECONDS_PER_DAY=60*60*24;
+static const uint32_t SECONDS_PER_DAY=60*60*24;
+static const uint32_t MINUTES_PER_DAY=60*24;
 
 void OctaveExporter::write_lead_in(const std::string& name,
     const std::string& description) {
   _out << "% script file containing sensor data of sensor " 
     << name << ", " << description << std::endl;
   _out << "1;" << std::endl;
+}
+
+static uint32_t calc_next_day_timestamp(uint32_t ts) {
+  // Use this formula to determine the beginning of the next day:
+  // $nextday = $current_timestamp + 86400 - ($current_timestamp % 86400);
+  return (ts + SECONDS_PER_DAY - (ts % SECONDS_PER_DAY));
 }
 
 void OctaveExporter::write_values_function(const std::string& name,
@@ -19,22 +28,49 @@ void OctaveExporter::write_values_function(const std::string& name,
   _out << "function values = get_" << name << "_values()" << std::endl;
   _out << "  values = [ ..." << std::endl;
 
-  // See http://www.cplusplus.com/reference/algorithm/min_element/
-  // $nextday = $current_timestamp + 86400 - ($current_timestamp % 86400);
+  std::vector<double> current_day(MINUTES_PER_DAY);
+  std::vector<double>::iterator day_it;
+  uint32_t next_day_timestamp=0;
 
-  // 1. determine position of first element in the day
+  // Look for the first reading and determine the first timestamp in the dataset. 
+  it = readings->begin(); 
+  klio::timestamp_t ts=(*it).first;
+  next_day_timestamp=calc_next_day_timestamp(ts);
 
-  // 2. Now, we can write out every value sequentially
-
-
-  for(  it = readings->begin(); it != readings->end(); it++) {
-    klio::timestamp_t ts1=(*it).first;
-    double val1=(*it).second;
-    std::cout << ts1 << "\t" << val1 << std::endl;
+  for( it = readings->begin(); it != readings->end(); it++) {
+    klio::timestamp_t ts=(*it).first;
+    double val=(*it).second;
+    //std::cout << ts << "\t" << val << std::endl;
+    if (ts >= next_day_timestamp) {
+  //    std::cout << " TS: " << ts << " next_day: " << next_day_timestamp << std::endl;
+      // write out the line
+      for (uint16_t i=0; i < MINUTES_PER_DAY; i++) {
+        _out << current_day[i];
+        if (i < MINUTES_PER_DAY-1)
+          _out << ", ";
+        else
+          _out << "; ..." << std::endl;
+      }
+      // clear the datastructures, setup for the next day
+      next_day_timestamp=calc_next_day_timestamp(ts+1);
+      current_day.clear();
+      current_day.resize(MINUTES_PER_DAY);
+    }
+    // Calculate position of this value within the day
+    std::vector<double>::size_type position = 
+      (ts - (next_day_timestamp - SECONDS_PER_DAY)) / 60;
+  //  std::cout << position << " / " << val << std::endl;
+    current_day[position]=val;
   }
-
-  
-
+  // Finish the last line - there is a difference: this one is not
+  // terminated by a ";" symbol.
+  for (uint16_t i=0; i < MINUTES_PER_DAY; i++) {
+    _out << current_day[i];
+    if (i < MINUTES_PER_DAY-1)
+      _out << ", ";
+    else
+      _out << " ..." << std::endl;
+  }
   _out << "  ]" << std::endl;
   _out << "end" << std::endl;
 }

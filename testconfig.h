8d64e5995ee2c370e9f32ac5fb331f6ff90367a9
elyoliveira <ely.oliveira@itwm.fraunhofer.de>
2014-02-20 09:12:18 +0100

    A constant has been renamed.

---
+++ testconfig.h
@@ -0,0 +1,31 @@
+/**
+ * This file is part of libklio.
+ *
+ * (c) Fraunhofer ITWM - Mathias Dalheimer <dalheimer@itwm.fhg.de>, 2010
+ *
+ * libklio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libklio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libklio. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef TESTS_TESTCONFIG_H_IN
+#define TESTS_TESTCONFIG_H_IN 1
+
+/* #undef ENABLE_LOGGING */
+#define TEST_DB1_FILE "/home/wagnerag/projects/libklio/tests/testdb1.sql"
+#define TEST_DB2_FILE "/home/wagnerag/projects/libklio/tests/testdb2.sql"
+#define TEST_DB3_FILE "/home/wagnerag/projects/libklio/tests/testdb3.sql"
+#define TEST_DB4_FILE "/home/wagnerag/projects/libklio/tests/testdb4.sql"
+#define TEST_DB_PATH "/home/wagnerag/projects/libklio/tests/testdb"
+
+#endif /* TESTS_TESTCONFIG_H_IN */

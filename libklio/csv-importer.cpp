689d83b6f54c324052f9ae6490c75e93ea1499ce
Ely de Oliveira <wagnerag@itwm.fraunhofer.de>
2014-07-29 15:55:16 +0200

    Command klio-import has been improved.

--- libklio/csv-importer.cpp
+++ libklio/csv-importer.cpp
@@ -1,6 +1,7 @@
 #include<fstream>
 #include <stdlib.h>
 #include <boost/tokenizer.hpp>
+#include <boost/lexical_cast.hpp>
 #include <libklio/time.hpp>
 #include "csv-importer.hpp"
 
@@ -11,35 +12,37 @@ readings_t_Ptr CSVImporter::process() {
 
     readings_t_Ptr readings(new readings_t());
     const TimeConverter::Ptr time_converter(new TimeConverter());
-    typedef boost::tokenizer<boost::char_separator<char> >  Tokenizer;
+    typedef boost::tokenizer<boost::char_separator<char> > Tokenizer;
 
     std::vector<std::string> record;
     std::string line;
-    int header_lines = 1;
 
     std::cout << "Importing readings using separator: " << _separator << std::endl;
     boost::char_separator<char> separator(_separator.c_str());
 
     while (getline(_in, line)) {
 
-        if (header_lines-- < 1 && !line.empty()) {
+        if (!line.empty()) {
 
             Tokenizer tokenizer(line, separator);
-            
+
             if (tokenizer.begin() != tokenizer.end()) {
 
                 record.assign(tokenizer.begin(), tokenizer.end());
 
-                if (record.size() > 1 && !record.at(0).empty() && !record.at(1).empty()) {
+                if (record.size() > 1) {
 
-                    timestamp_t timestamp = time_converter->convert_from_epoch(atoi(record.at(0).c_str()));
-
-                    if (timestamp > 0) {
-
-                        double reading = atof(record.at(1).c_str());
+                    try {
+                        timestamp_t timestamp = boost::lexical_cast<timestamp_t>(record.at(0));
+                        double reading = boost::lexical_cast<double>(record.at(1));
                         std::cout << timestamp << ": " << reading << std::endl;
 
-                        readings->insert(std::pair<timestamp_t, double>(timestamp, reading));
+                        readings->insert(std::pair<timestamp_t, double>(
+                                time_converter->convert_from_epoch(atoi(record.at(0).c_str())),
+                                reading));
+
+                    } catch (boost::bad_lexical_cast &) {
+                        //Ignore
                     }
                 }
             }

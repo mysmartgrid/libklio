ab4de027651970747de6869b2fd65582d73e218b
Kai Krueger <kai.krueger@itwm.fraunhofer.de>
2014-11-17 16:43:55 +0100

    Make storetest link when libmysmartgrid does not exists

---
+++ tests/store/main.cpp
@@ -0,0 +1,48 @@
+/**
+ * This file is part of libklio.
+ *
+ * (c) Fraunhofer ITWM - Mathias Dalheimer <dalheimer@itwm.fhg.de>,    2010
+ *                       Ely de Oliveira   <ely.oliveira@itwm.fhg.de>, 2013
+ *
+ * libklio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libklio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libklio. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * this file only exists to define the BOOST_TEST_MODULE name. All the
+ * test codes is in the other .cpp files in this directory.
+ */
+#define BOOST_TEST_MODULE msg_store_test
+
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+
+
+/**
+ * see http://www.boost.org/doc/libs/1_43_0/libs/test/doc/html/tutorials/hello-the-testing-world.html
+ */
+
+BOOST_AUTO_TEST_CASE ( check_sanity ) {
+  try {
+    std::cout << std::endl << "*** Demo test case: Checking world sanity." << std::endl;
+    BOOST_CHECK_EQUAL (42, 42);
+    BOOST_CHECK( 23 != 42 );        // #1 continues on error
+    BOOST_REQUIRE( 23 != 42 );      // #2 throws on error
+    std::cout << "The world seems to be functioning. For now." << std::endl;
+  } catch (std::exception const & ex) {
+    BOOST_ERROR ( ex.what() );
+  }
+  if( 23 == 42 ) {
+    BOOST_FAIL( "23 == 42, oh noes");             // #4 throws on error
+  }
+}
